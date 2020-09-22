3702b2e102d068d794f69b391a98b1ee19cc24a2
Gaurav Soni <45313227+ergauravsoni@users.noreply.github.com>
2020-09-22 17:41:13 +0530

    upto application building

---
+++ app.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, request, render_template
+from sklearn.feature_extraction.text import CountVectorizer
+from joblib import load
+app = Flask(__name__)
+from tensorflow import keras
+model = keras.models.load_model('model1.h5')
+
+@app.route('/')
+def home():
+    return render_template('homepage.html')
+    
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/y_predict',methods=['POST'])
+def y_predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    da = [[x for x in request.form.values()]]
+    da = da[0][0]
+    print(da)
+    
+    loaded=CountVectorizer(decode_error='replace',vocabulary=load('features.save'))
+    
+    da=da.split('delimeter')
+    result=model.predict(loaded.transform(da))
+    print(result)
+    if result>=0.5:
+        output="Real"
+    else:
+        output="Fake"
+        
+    
+    return render_template('index.html', prediction_text='This is a {} review'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
